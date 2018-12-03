83038216c3903d1bfd73e10c91d45b7a316c5bc2
Silknes <eliott.dupau@gmail.com>
2018-12-03 07:06:25 +0100

    Configure the NavigationDrawer

--- app/src/main/java/com/oc/eliott/go4lunch/Controller/Activities/MainActivity.java
+++ app/src/main/java/com/oc/eliott/go4lunch/Controller/Activities/MainActivity.java
@@ -2,6 +2,10 @@ package com.oc.eliott.go4lunch.Controller.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,7 +13,9 @@ import android.widget.Toast;
 
 import com.oc.eliott.go4lunch.R;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,9 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 
         this.configureToolbar();
-        this.updateUIWhenCreating();
+        this.configureDrawerLayout();
+        this.configureNavigationView();
+        //this.updateUIWhenCreating();
     }
 
     // Starting AuthentificationActivity if current user not log
@@ -52,7 +60,52 @@ public class MainActivity extends BaseActivity {
 
     // Use to setup the new toolbar
     private void configureToolbar(){
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        this.toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    // This method closed the NavigationDrawer when user click on back button
+    @Override
+    public void onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    // Configure the click on each item of the navigation drawer
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.item_your_lunch :
+                Toast.makeText(this, "Your Lunch", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item_logout:
+                Toast.makeText(this, "LogOut", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    // This method set a new icon on the toolbar which open the NavigationDrawer
+    private void configureDrawerLayout(){
+        this.drawerLayout = findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // This method configure the NavigationView and add a listener on each item
+    private void configureNavigationView(){
+        NavigationView navigationView = findViewById(R.id.activity_main_navigation_drawer);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 }
